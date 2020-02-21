da74bb39ff76afe5af51ce81f79c5f0e74160c1b
nigamabhay671 <nigamabhay671@users.noreply.github.com>
2020-02-21 23:33:38 +0530

    Add project files.

---
+++ WithoutEncapsulationDemo/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WithoutEncapsulationDemo
+{
+    class Employee
+    {
+        public int Id;
+        public string Name;
+        public int Salary;
+    }
+}
---
+++ WithoutEncapsulationDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WithoutEncapsulationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+            int a = Employee.Id = 1;
+            string b = Employee.Name = "abhay";
+            int c = Employee.Salary = 10000;
+            Console.WriteLine(a + " " + b + " " + c);
+        }
+    }
+}
